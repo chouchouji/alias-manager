55c31875e1771827efa6470cc657e83aa176d0b7
chouchouji <1305974212@qq.com>
2024-11-10 18:57:32 +0800

    chore: optimize tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/extension.ts'],
   external: ['vscode'],
+  noExternal: ['rattail'],
   splitting: false,
   clean: true,
   format: ['cjs'],
