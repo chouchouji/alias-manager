339eefe3c54c4aaf7a7f1b5b0aea013e1ec4ba8f
chouchouji <1305974212@qq.com>
2025-03-10 17:10:56 +0800

    fix: don't allow to add duplicate alias to the group

--- src/extension.ts
+++ src/extension.ts
@@ -594,7 +594,7 @@ alias ${alias}`,
       return
     }
 
-    const groups = this.globalState.keys().filter((key) => ![SYSTEM_ALIAS, alias.group].includes(key))
+    const groups = this.globalState.keys().filter((group) => ![SYSTEM_ALIAS, alias.group].includes(group))
     if (groups.length === 0) {
       vscode.window.showWarningMessage('No any group can be added')
       return
@@ -610,6 +610,12 @@ alias ${alias}`,
     }
 
     const aliases = normalizeAliasesToArray<Alias>(this.globalState.get(selectedGroup))
+    const hasSameAlias = aliases.some((aliasItem) => isSameAlias(alias.data!, aliasItem))
+    if (hasSameAlias) {
+      vscode.window.showInformationMessage(vscode.l10n.t('This alias already exists'))
+      return
+    }
+
     aliases.push(alias.data)
     this.globalState.update(selectedGroup, aliases)
 
